89db6dd2577d8139168ab0a9173fe349af6724a5
Michael <michaelandhsm2@gmail.com>
2016-11-29 19:49:55 +0800

    Root set, pages routing working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  root to: 'static_pages#home'
+
+  get '/home', to: 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
