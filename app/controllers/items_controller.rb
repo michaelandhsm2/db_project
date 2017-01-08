bc12c541f279219d165a3b66f76e6f201a24fd76
Michael <michaelandhsm2@gmail.com>
2017-01-08 09:54:26 +0800

    Advanced Event Model Done - less personal flavors

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,11 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @item.coupons.each do |e|
+      if e.start_date < DateTime.current && e.end_date > DateTime.current
+        flash_message_now :info, "#{e.event.name} - You will get $#{e.free_cash} free for every $#{e.minimum_spending} spent on items like this between #{e.start_date.strftime("%b %e, %C%y")} and #{e.end_date.strftime("%b %e, %C%y")}."
+      end
+    end
     @item.specials.each do |e|
       if e.start_date < DateTime.current && e.end_date > DateTime.current
         flash_message_now :info, "#{e.event.name} - '#{@item.name}' is currently #{e.discount}% off between #{e.start_date.strftime("%b %e, %C%y")} and #{e.end_date.strftime("%b %e, %C%y")}."
