f2bdc98fc74e38cdf2c91a00bfe652c816482f61
leon20121005 <leon20121005@gmail.com>
2017-01-09 01:32:34 +0800

    implement authorization of user's cart, order, admin namespace order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,6 @@
 class Admin::OrdersController < ApplicationController
+  before_action :is_admin?
+
   def index
     @orders = Order.paginate(page: params[:page], :per_page => 10)
   end
@@ -7,4 +9,13 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_items = @order.order_items
   end
+
+  private
+
+    def is_admin?
+      unless current_user.is_admin
+        flash[:danger] =  "你的身分並非管理者"
+        redirect_to root_url
+      end
+    end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user
+
   def index
     @orders = current_user.orders.paginate(page: params[:page], :per_page => 10)
   end
@@ -7,4 +10,19 @@ class OrdersController < ApplicationController
     @order = current_user.orders.find(params[:id])
     @order_items = @order.order_items
   end
+
+  private
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "請先登入會員"
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
