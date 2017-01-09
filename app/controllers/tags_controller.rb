ba68da0b681e25309a3ba7ab740450a929e202bf
Michael <michaelandhsm2@gmail.com>
2017-01-10 07:57:15 +0800

    Minor text changes

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,7 @@
 class TagsController < ApplicationController
   def show
     @item = Item.tagged_with(params[:label]).paginate(page: params[:page], :per_page => 8)
+    @tag = params[:label]
   end
 
   def index
